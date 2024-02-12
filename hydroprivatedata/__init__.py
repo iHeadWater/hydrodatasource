c07f1ca7292b52498f0bd8b8c3e7358319664831
ouyangwenyu <wenyuouyang@outlook.com>
2024-02-12 13:02:54 +0800

    merge content from hydro_opendata but need refactor

--- hydroprivatedata/__init__.py
+++ hydroprivatedata/__init__.py
@@ -1,5 +1,23 @@
 """Top-level package for hydro_privatedata."""
 
-__author__ = """Wenyu Ouyang"""
-__email__ = 'hust2014owen@gmail.com'
-__version__ = '0.0.1'
+import pathlib
+import os
+
+from .common import minio_paras
+
+
+__author__ = """Jianfeng Zhu"""
+__email__ = "zjf014@gmail.com"
+__version__ = "0.0.1"
+
+home_path = str(pathlib.Path.home())
+
+if not os.path.exists(os.path.join(home_path, ".wisminio")):
+    access_key = input("minio_access_key:")
+    secret_key = input("minio_secret_key:")
+
+    with open(os.path.join(home_path, ".wisminio"), "w") as f:
+        f.write("endpoint_url = " + minio_paras["endpoint_url"])
+        f.write("\naccess_key = " + access_key)
+        f.write("\nsecret_key = " + secret_key)
+        f.write("\nbucket_name = " + minio_paras["bucket_name"])
