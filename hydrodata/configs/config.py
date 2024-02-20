5f033a94f46dac9e78055452e40ef1525f37ae18
“silencesoup <silencesoup@outlook.com>
2024-02-20 11:34:19 +0800

    delete useless config, and initialize SETTING before try except, because it causes output error in test module

--- hydrodata/configs/config.py
+++ hydrodata/configs/config.py
@@ -65,6 +65,7 @@ def read_setting(setting_path):
 
 
 SETTING_FILE = os.path.join(Path.home(), "hydro_setting.yml")
+SETTING = {}
 try:
     SETTING = read_setting(SETTING_FILE)
 except ValueError as e:
@@ -114,53 +115,3 @@ STATION_BUCKET = "stations"
 STATION_OBJECT = "sites.csv"
 
 GRID_INTERIM_BUCKET = "grids-interim"
-
-
-class DataConfig:
-    def __init__(self):
-        self.config = {
-            # 是否从hydrodata读取GPM_GFS文件
-            "GPM_GFS_local_read": False,
-            "GPM_GFS_merge": True,
-            # 如果从本地读，需要给定GPM_GFS的路径,upload和local代表是否上传到Minio和是否下载到本地
-            "GPM_GFS_local_path": "/home/xushuolong1/hydrodata/data",
-            "GPM_GFS_upload": False,
-            "GPM_GFS_local_save": True,
-            # 是否需要从源数据合并新数据
-            "GFS_merge": True,
-            # 如果从本地读，需要给定GPM或者GFS的路径
-            "GFS_local_path": "/home/xushuolong1/hydrodata/data/gfs.nc",
-            # 数据类型，目前只有降水
-            "data_type": "tp",
-            # 拼接后的gpm_gfs的step中，GPM数据占据前多少个step
-            "GPM_length": 169,
-            # 拼接后的gpm_gfs的step中，GFS数据占据后多少个step
-            "GFS_length": 23,
-            # 拼接后的gpm_gfs的time_now，对应step中的第多少个值
-            "time_now": 168,
-            # 拼接后的gpm_gfs的time_now的时间范围
-            "time_periods": [
-                ["2017-01-10T00:00:00", "2017-01-11T00:00:00"],
-                ["2017-01-15T00:00:00", "2017-01-20T00:00:00"],
-            ],
-            # 是否从hydrodata重新读取并生成gpm.nc，是否上传，是否保存在本地
-            # GFS基本同理，额外会有两个参数
-            # enlarge参数，是因为GPM和GFS拼接的时候，需要GFS比GPM大一圈才能插值，不然会出现-
-            # new参数，这个是为了模拟实际预报，因为实际预报的时候，GFS的数据不会是每一个时刻最新的数据
-            # 但考虑到有时候算其他数据不需要大一圈，因此这里写了个参数
-            "GFS_enlarge": True,
-            "GFS_new": True,
-            "GFS_upload": False,
-            "GFS_local_save": True,
-            # 如果重新读取并生成gpm.nc，且没有mask.nc，那么需要提供GPM_mask，GFS同理
-            "GFS_mask": False,
-            "GFS_mask_path": "/home/xushuolong1/hydrodata/data/mask",
-            # 如果需要生成mask.nc，那么需要提供流域的shp文件
-            # 如果生成GPM.nc，那么需要提供时间范围，理论上最好是连续的，以防特殊需求或者特殊情况，这里还是写成了分段的，GFS同理
-            "GFS_time_periods": [
-                ["2017-01-01T00:00:00", "2017-01-31T00:00:00"],
-            ],
-        }
-
-    def get_config(self):
-        return self.config
