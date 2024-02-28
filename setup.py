87667ae66ad5bdbe4f58ad38782ed57968608bfe
forestbat <k678900000@163.com>
2024-02-28 19:12:41 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     install_requires=install_requires,
     dependency_links=dependency_links,
     license="BSD license",
-    long_description=readme,
+    # long_description=readme,
     long_description_content_type='text/markdown',
     include_package_data=True,
     keywords='hydrodata',
