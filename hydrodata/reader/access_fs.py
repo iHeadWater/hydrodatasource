57407d653bd6490a24e73eeb032e0395f0ebbe63
zaixiaxiaowu <458206081@qq.com>
2024-03-14 09:36:16 +0800

    fix bug in read_valid_data

--- hydrodata/reader/access_fs.py
+++ hydrodata/reader/access_fs.py
@@ -13,28 +13,34 @@ from kerchunk.hdf import SingleHdf5ToZarr
 import hydrodata.configs.config as conf
 
 
-def spec_path(url_path: str, head='local', need_cache=False, is_dir=False):
+def spec_path(url_path: str, head="local", need_cache=False, is_dir=False):
     if is_dir is False:
-        if head == 'local':
+        if head == "local":
             url_path = os.path.join(conf.LOCAL_DATA_PATH, url_path)
             ret_data = read_valid_data(url_path, need_cache=need_cache)
-        elif head == 'minio':
-            url_path = 's3://' + url_path
-            ret_data = read_valid_data(url_path, storage_option=conf.MINIO_PARAM, need_cache=need_cache)
+        elif head == "minio":
+            url_path = "s3://" + url_path
+            ret_data = read_valid_data(
+                url_path, storage_option=conf.MINIO_PARAM, need_cache=need_cache
+            )
         else:
             raise ValueError("head should be 'local' or 'minio'")
     else:
         ret_data = []
-        if head == 'local':
+        if head == "local":
             url_path = os.path.join(conf.LOCAL_DATA_PATH, url_path)
-            for file in glob.glob(url_path + '/**', recursive=True):
+            for file in glob.glob(url_path + "/**", recursive=True):
                 if not os.path.isdir(file):
                     ret_data.append(read_valid_data(file, need_cache=need_cache))
-        elif head == 'minio':
-            url_path = 's3://' + url_path
-            for file in conf.FS.glob(url_path + '/**'):
+        elif head == "minio":
+            url_path = "s3://" + url_path
+            for file in conf.FS.glob(url_path + "/**"):
                 if not conf.FS.isdir(file):
-                    ret_data.append(read_valid_data(file, storage_option=conf.MINIO_PARAM, need_cache=need_cache))
+                    ret_data.append(
+                        read_valid_data(
+                            file, storage_option=conf.MINIO_PARAM, need_cache=need_cache
+                        )
+                    )
         else:
             raise ValueError("head should be 'local' or 'minio'")
     return ret_data
@@ -56,38 +62,41 @@ def read_valid_data(obj: str, storage_option=None, need_cache=False, need_refer=
     object: The data object.
     """
     data_obj = None
-    dot_in_obj = '.' in obj
-    cache_name = obj.lstrip('s3://').split('/')[-1]
+    dot_in_obj = "." in obj
+    cache_name = obj.lstrip("s3://").split("/")[-1]
     if not dot_in_obj:
         data_obj = pd.read_fwf(obj, storage_options=storage_option)
         if (need_cache is True) & (storage_option is not None):
             data_obj.to_file(path=os.path.join(conf.LOCAL_DATA_PATH, cache_name))
     elif dot_in_obj:
-        ext_name = obj.split('.')[-1]
-        if ext_name == 'csv':
+        ext_name = obj.split(".")[-1]
+        if ext_name == "csv":
             data_obj = pd.read_csv(obj, storage_options=storage_option)
             if (need_cache is True) & (storage_option is not None):
                 data_obj.to_csv(path=os.path.join(conf.LOCAL_DATA_PATH, cache_name))
-        elif (ext_name == 'nc') or (ext_name == 'nc4') or (ext_name == 'hdf5'):
+        elif (ext_name == "nc") or (ext_name == "nc4") or (ext_name == "hdf5"):
             if need_refer is True:
                 data_obj = gen_refer_and_read_zarr(obj, storage_option=storage_option)
             else:
-                nc_source = intk.datatypes.HDF5(obj, storage_options=storage_option)
-                nc_src_reader = intk.readers.DaskHDF(nc_source).to_reader()
-                data_obj: xr.Dataset = nc_src_reader.read()
+                # nc_source = intk.datatypes.HDF5(obj, storage_options=storage_option)
+                # nc_src_reader = intk.readers.DaskHDF(nc_source).to_reader()
+                # data_obj: xr.Dataset = nc_src_reader.read()
+                data_obj = xr.open_dataset(conf.FS.open(obj), chunks="auto")
             if (need_cache is True) & (storage_option is not None):
                 data_obj.to_netcdf(path=os.path.join(conf.LOCAL_DATA_PATH, cache_name))
-        elif ext_name == 'json':
+        elif ext_name == "json":
             data_obj = pd.read_json(obj, storage_options=storage_option)
             if (need_cache is True) & (storage_option is not None):
-                data_obj.to_json(path_or_buf=os.path.join(conf.LOCAL_DATA_PATH, cache_name))
-        elif ext_name == 'shp':
+                data_obj.to_json(
+                    path_or_buf=os.path.join(conf.LOCAL_DATA_PATH, cache_name)
+                )
+        elif ext_name == "shp":
             # Can't run directly, see this: https://github.com/geopandas/geopandas/issues/3129
             remote_shp_obj = conf.FS.open(obj)
-            data_obj = gpd.read_file(remote_shp_obj, engine='pyogrio')
+            data_obj = gpd.read_file(remote_shp_obj, engine="pyogrio")
             if (need_cache is True) & (storage_option is not None):
                 data_obj.to_file(path=os.path.join(conf.LOCAL_DATA_PATH, cache_name))
-        elif 'grb2' in obj:
+        elif "grb2" in obj:
             # ValueError: unrecognized engine cfgrib must be one of: ['netcdf4', 'h5netcdf', 'scipy', 'store', 'zarr']
             # https://blog.csdn.net/weixin_44052055/article/details/108658464?spm=1001.2014.3001.5501
             # 似乎只能用conda来装eccodes
@@ -95,16 +104,16 @@ def read_valid_data(obj: str, storage_option=None, need_cache=False, need_refer=
             grib_ds = xr.open_dataset(remote_grib_obj)
             if (need_cache is True) & (storage_option is not None):
                 grib_ds.to_netcdf(path=os.path.join(conf.LOCAL_DATA_PATH, cache_name))
-        elif ext_name == 'txt':
+        elif ext_name == "txt":
             data_obj = pd.read_fwf(obj, storage_options=storage_option)
             if (need_cache is True) & (storage_option is not None):
                 data_obj.to_csv(os.path.join(conf.LOCAL_DATA_PATH, cache_name))
-        elif ext_name == '.zarr':
+        elif ext_name == ".zarr":
             zarr_mapper = conf.FS.get_mapper(obj)
             zarr_store = zarr.storage.KVStore(zarr_mapper)
             data_obj = xr.open_zarr(zarr_store)
         else:
-            logging.error(f'Unsupported file type: {ext_name}')
+            logging.error(f"Unsupported file type: {ext_name}")
     else:
         data_obj = object()
         logging.error("这是数据存储，不是百度云盘！")
@@ -113,9 +122,9 @@ def read_valid_data(obj: str, storage_option=None, need_cache=False, need_refer=
 
 def gen_refer_and_read_zarr(obj_path, storage_option=None):
     # 有问题尚未解决
-    obj_json_path = 's3://references/' + str(obj_path) + '.json'
+    obj_json_path = "s3://references/" + str(obj_path) + ".json"
     if not conf.FS.exists(obj_json_path):
-        with open(obj_path, 'wb') as fpj:
+        with open(obj_path, "wb") as fpj:
             nc_chunks = SingleHdf5ToZarr(h5f=obj_path)
             fpj.write(ujson.dumps(nc_chunks.translate()).encode())
     data_type_obj = intk.datatypes.HDF5(obj_json_path, storage_options=storage_option)
@@ -124,6 +133,8 @@ def gen_refer_and_read_zarr(obj_path, storage_option=None):
     return data_obj
 
 
-def filter_area_from_minio(aoi_type, aoi_param, data_name, state='origin', need_cache=False):
+def filter_area_from_minio(
+    aoi_type, aoi_param, data_name, state="origin", need_cache=False
+):
     # todo
-    minio_path = aoi_type + '/' + aoi_param + '/' + data_name
+    minio_path = aoi_type + "/" + aoi_param + "/" + data_name
--- tests/test_spec_path.py
+++ tests/test_spec_path.py
@@ -3,5 +3,8 @@ from hydrodata.reader import access_fs
 
 def test_read_spec():
     # access_fs.spec_path("st_rain_c.csv")
-    # gpm_gfs_nc = access_fs.spec_path('grids-interim/86_21401550/gpm_gfs.nc', head='minio')
-    print_json = access_fs.spec_path('test/geodata/era5_land/era5_land_.json', head='minio', need_cache=False)
+    mean_forcing_nc = access_fs.spec_path(
+        "basins-origin/hour_data/1h/mean_data/mean_data_forcing/mean_forcing_CHN_21401550.nc",
+        head="minio",
+    )
+    print(mean_forcing_nc)
