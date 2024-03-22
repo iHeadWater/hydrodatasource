6398de35e4a00041a7c148e3491d828c4564a212
zaixiaxiaowu <458206081@qq.com>
2024-03-22 17:25:19 +0800

    add read_zz_station_ts

--- tests/test_spec_path.py
+++ tests/test_spec_path.py
@@ -49,14 +49,14 @@ def test_read_pp_stations_csv():
     )
     print(pp_stations)
 
+
 def test_read_pp_stations_shp():
     # 读取zip中的shpfiles文件
     pp_stations = gpd.read_file(
-        conf.FS.open(
-            "s3://stations-origin/stations_list/pp_stations.zip"
-        )
+        conf.FS.open("s3://stations-origin/stations_list/pp_stations.zip")
     )
     print(pp_stations)
+
 
 def test_read_zz_stations_csv():
     # 读取csv文件
@@ -66,14 +66,24 @@ def test_read_zz_stations_csv():
     )
     print(zz_stations)
 
+
+def test_read_zz_stations_ts():
+    # 读取csv文件
+    zz_stations = access_fs.spec_path(
+        "stations-origin/zz_stations/hour_data/1h/zz_CHN_songliao_10800100.csv",
+        head="minio",
+    )
+    print(zz_stations)
+
+
 def test_read_zz_stations_shp():
     # 读取zip中的shpfiles文件
     zz_stations = gpd.read_file(
-        conf.FS.open(
-            "s3://stations-origin/stations_list/zz_stations.zip"
-        )
+        conf.FS.open("s3://stations-origin/stations_list/zz_stations.zip")
     )
     print(zz_stations)
+
+
 def test_read_zq_stations_csv():
     # 读取csv文件
     zq_stations = access_fs.spec_path(
@@ -82,20 +92,28 @@ def test_read_zq_stations_csv():
     )
     print(zq_stations)
 
+
 def test_read_zq_stations_shp():
     # 读取zip中的shpfiles文件
     zq_stations = gpd.read_file(
-        conf.FS.open(
-            "s3://stations-origin/stations_list/zq_stations.zip"
-        )
+        conf.FS.open("s3://stations-origin/stations_list/zq_stations.zip")
     )
     print(zq_stations)
 
+
 def test_read_recovered_data():
     import pandas as pd
-    test_csv = pd.read_csv('s3://stations-interim/zq_stations/zq_CHN_songliao_10310500.csv', storage_options=conf.MINIO_PARAM)
-    zq_csv = pd.read_csv('s3://stations-interim/zq_stations.csv', storage_options=conf.MINIO_PARAM)
-    dams_gdf = gpd.read_file(conf.FS.open('s3://reservoirs-origin/dams.zip'))
-    rsvrs_gdf = gpd.read_file(conf.FS.open('s3://reservoirs-origin/rsvrs_shp.zip'))
-    rr_df = pd.read_csv('s3://reservoirs-origin/rr_stations/zq_CHN_songliao_10310500.csv', storage_options=conf.MINIO_PARAM)
-    
+
+    test_csv = pd.read_csv(
+        "s3://stations-interim/zq_stations/zq_CHN_songliao_10310500.csv",
+        storage_options=conf.MINIO_PARAM,
+    )
+    zq_csv = pd.read_csv(
+        "s3://stations-interim/zq_stations.csv", storage_options=conf.MINIO_PARAM
+    )
+    dams_gdf = gpd.read_file(conf.FS.open("s3://reservoirs-origin/dams.zip"))
+    rsvrs_gdf = gpd.read_file(conf.FS.open("s3://reservoirs-origin/rsvrs_shp.zip"))
+    rr_df = pd.read_csv(
+        "s3://reservoirs-origin/rr_stations/zq_CHN_songliao_10310500.csv",
+        storage_options=conf.MINIO_PARAM,
+    )
