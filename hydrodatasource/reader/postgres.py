3d0892b351840345901160b961877b610f3b4b36
XU SHUOLONG <silencesoup@outlook.com>
2024-05-28 11:53:24 +0800

    postgres read forcing data

--- hydrodatasource/reader/postgres.py
+++ hydrodatasource/reader/postgres.py
@@ -1,21 +1,41 @@
 from hydrodatasource.configs.config import PS
 from datetime import datetime
 
-def read_data(stcd, datatype, start_time, end_time):
+
+def read_forcing(var_type, basin, start_time):
+    if start_time is None:
+        raise ValueError("start_time cannot be None")
+
+    table_name = {
+        "gpm": "t_gpm_pre_data",
+        "gfs_tp": "t_gfs_tp_pre_data",
+        "smap": "t_smap_pre_data",
+        "gfs_sm": "t_gfs_soil_pre_data",
+    }
+
+    # 连接数据库
     ps = PS.cursor()
-    dt_start = datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%S")
-    dt_end = datetime.strptime(end_time, "%Y-%m-%dT%H:%M:%S")
-    dt_start_time = dt_start.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]
-    dt_end_time = dt_end.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]
-    if datatype == 'rain':
-        table = 'st_pptn_r'
-        columns = "tm, intv, drp"
-    elif datatype == 'streamflow':
-        table = 'st_rsvr_r'
-        columns = "tm, inq"
-    else:
-        raise ValueError("datatype must be 'rain' or 'streamflow'")
-    sql = f"SELECT {columns} FROM {table} where stcd = %s and tm between %s and %s"
+
+    if var_type not in table_name:
+        raise ValueError("var_type must be one of 'gpm', 'gfs_tp', 'smap', 'gfs_sm'")
+
+    if var_type in ["gpm", "smap"]:
+        sql = f"""
+        SELECT * FROM {table_name[var_type]}
+        WHERE predictdate >= %s AND basincode = %s
+        """
+    elif var_type in ["gfs_tp", "gfs_sm"]:
+        sql = f"""
+        SELECT * FROM {table_name[var_type]}
+        WHERE forecastdatetime >= %s AND basin_code = %s
+        """
+
+    # 执行查询
     PS.autocommit = True
-    ps.execute(sql, (stcd, dt_start_time, dt_end_time))
-    return ps.fetchall()
+    ps.execute(sql, (start_time, basin))
+    results = ps.fetchall()
+
+    # 关闭游标和连接
+    ps.close()
+
+    return results
