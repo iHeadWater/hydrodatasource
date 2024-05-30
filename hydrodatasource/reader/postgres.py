fbefb61314e264afc847ed98f5df75efe701a4e8
XU SHUOLONG <silencesoup@outlook.com>
2024-05-30 15:20:55 +0800

    delete useless function

--- hydrodatasource/reader/postgres.py
+++ hydrodatasource/reader/postgres.py
@@ -1,135 +1,8 @@
-from hydrodatasource.configs.config import SETTING, PS
+from hydrodatasource.configs.config import SETTING
 from datetime import datetime
 import pandas as pd
 from loguru import logger
 from sqlalchemy import create_engine
-
-
-def read_forcing(var_type, basin, start_time):
-    if start_time is None:
-        raise ValueError("start_time cannot be None")
-
-    table_name = {
-        "gpm_tp": "t_gpm_pre_data",
-        "gfs_tp": "t_gfs_tp_pre_data",
-        "smap": "t_smap_pre_data",
-        "gfs_soil": "t_gfs_soil_pre_data",
-    }
-
-    # 连接数据库
-    ps = PS.cursor()
-
-    if var_type not in table_name:
-        raise ValueError(
-            "var_type must be one of 'gpm_tp', 'gfs_tp', 'smap', 'gfs_soil'"
-        )
-
-    if var_type == "gpm_tp":
-        sql = f"""
-        SELECT 
-            basincode, 
-            predictdate, 
-            data ->> 'tp' AS tp,
-            data ->> 'raster_area' AS raster_area,
-            data ->> 'intersection_area' AS intersection_area
-        FROM (
-            SELECT 
-                basincode, 
-                predictdate, 
-                jsonb_array_elements(data) AS data
-            FROM {table_name[var_type]}
-        ) {table_name[var_type]}
-        WHERE predictdate >= %s AND basincode = %s
-        """
-    elif var_type == "smap":
-        sql = f"""
-        SELECT 
-            basincode, 
-            predictdate, 
-            data ->> 'sm_surface' AS sm_surface,
-            data ->> 'raster_area' AS raster_area,
-            data ->> 'intersection_area' AS intersection_area
-        FROM (
-            SELECT 
-                basincode, 
-                predictdate, 
-                jsonb_array_elements(data) AS data
-            FROM {table_name[var_type]}
-        ) {table_name[var_type]}
-        WHERE predictdate >= %s AND basincode = %s
-        """
-    elif var_type == "gfs_tp":
-        sql = f"""
-        select
-            basin_code,
-            forecastdatetime,
-            tp,
-            raster_area,
-            intersection_area 
-        from {table_name[var_type]}
-        where forecastdatetime >= %s and basin_code = %s
-        """
-    elif var_type == "gfs_soil":
-        sql = f"""
-        select
-            basin_code,
-            forecastdatetime,
-            soilw,
-            raster_area,
-            intersection_area 
-        from {table_name[var_type]}
-        where forecastdatetime >= %s and basin_code = %s
-        """
-
-    # 执行查询
-    PS.autocommit = True
-    ps.execute(sql, (start_time, basin))
-    results = ps.fetchall()
-
-    # 关闭游标和连接
-    ps.close()
-
-    return results
-
-
-def get_forcing_dataframe(var_type, basin, start_time):
-    data = read_forcing(var_type, basin, start_time)
-    column_dataname = {
-        "gpm_tp": "tp",
-        "smap": "sm_surface",
-        "gfs_tp": "tp",
-        "gfs_soil": "soilw",
-    }
-    if var_type in ["gpm_tp", "smap"]:
-        columns = [
-            "basincode",
-            "predictdate",
-            column_dataname[var_type],
-            "raster_area",
-            "intersection_area",
-        ]
-        datetime_column = "predictdate"
-    elif var_type in ["gfs_tp", "gfs_soil"]:
-        columns = [
-            "basin_code",
-            "forecastdatetime",
-            column_dataname[var_type],
-            "raster_area",
-            "intersection_area",
-        ]
-        datetime_column = "forecastdatetime"
-
-    df = pd.DataFrame(data, columns=columns)
-
-    # 转换数据类型
-    df[column_dataname[var_type]] = df[column_dataname[var_type]].astype(float)
-    df["raster_area"] = df["raster_area"].astype(float)
-    df["intersection_area"] = df["intersection_area"].astype(float)
-
-    # 按照时间列排序
-    df = df.sort_values(by=datetime_column)
-
-    return df
 
 
 def read_forcing_dataframe(var_type, basin, start_time):
@@ -142,6 +15,32 @@ def read_forcing_dataframe(var_type, basin, start_time):
         "smap": "t_smap_pre_data",
         "gfs_soil": "t_gfs_soil_pre_data",
     }
+
+    column_dataname = {
+        "gpm_tp": "tp",
+        "smap": "sm_surface",
+        "gfs_tp": "tp",
+        "gfs_soil": "soilw",
+    }
+
+    if var_type in ["gpm_tp", "smap"]:
+        columns_mapping = {
+            "basincode": "basincode",
+            "predictdate": "predictdate",
+            column_dataname[var_type]: column_dataname[var_type],
+            "raster_area": "raster_area",
+            "intersection_area": "intersection_area",
+        }
+        datetime_column = "predictdate"
+    elif var_type in ["gfs_tp", "gfs_soil"]:
+        columns_mapping = {
+            "basin_code": "basin_code",
+            "forecastdatetime": "forecastdatetime",
+            column_dataname[var_type]: column_dataname[var_type],
+            "raster_area": "raster_area",
+            "intersection_area": "intersection_area",
+        }
+        datetime_column = "forecastdatetime"
 
     if var_type not in table_name:
         raise ValueError(
@@ -215,9 +114,16 @@ def read_forcing_dataframe(var_type, basin, start_time):
     )
     # 执行查询数据SQL查询
     try:
-        result = pd.read_sql(sql, engine)
+        df = pd.read_sql(sql, engine)
+        df = df.rename(columns=columns_mapping)
+        # 转换数据类型
+        df[column_dataname[var_type]] = df[column_dataname[var_type]].astype(float)
+        df["raster_area"] = df["raster_area"].astype(float)
+        df["intersection_area"] = df["intersection_area"].astype(float)
+        # 按照时间列排序
+        df = df.sort_values(by=datetime_column)
     except Exception as e:
         logger.error(e)
         raise Exception()
 
-    return result
+    return df
--- tests/test_read_postgres.py
+++ tests/test_read_postgres.py
@@ -1,35 +1,24 @@
 from hydrodatasource.reader.postgres import (
-    read_forcing,
-    get_forcing_dataframe,
     read_forcing_dataframe,
 )
 
 
 def test_read_gpm():
-    data = read_forcing("gpm_tp", "21401550", "2024-05-30 00:00:00")
+    data = read_forcing_dataframe("gpm_tp", "21401550", "2024-05-30 00:00:00")
     print(data)
 
 
 def test_read_gfs_tp():
-    data = read_forcing("gfs_tp", "21401550", "2024-05-30 00:00:00")
+    data = read_forcing_dataframe("gfs_tp", "21401550", "2024-05-20 00:00:00")
     print(data)
+    data.to_csv("test.csv")
 
 
 def test_read_smap():
-    data = read_forcing("smap", "21401550", "2024-05-20 14:00:00")
+    data = read_forcing_dataframe("smap", "21401550", "2024-05-20 14:00:00")
     print(data)
 
 
 def test_read_gfs_sm():
-    data = read_forcing("gfs_soil", "21401550", "2024-05-20 19:00:00")
-    print(data)
-
-
-def test_get_dataframe_gpm():
-    data = get_forcing_dataframe("gpm_tp", "21401550", "2024-05-24 00:00:00")
-    print(data)
-
-
-def test_read_gpm_dataframe():
-    data = read_forcing_dataframe("gpm_tp", "21401550", "2024-05-24 00:00:00")
+    data = read_forcing_dataframe("gfs_soil", "21401550", "2024-05-20 19:00:00")
     print(data)
