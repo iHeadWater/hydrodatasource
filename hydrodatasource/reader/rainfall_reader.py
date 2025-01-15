19a36516f110243950e8196c7cbb12281d23fb97
owen <wenyuouyang@outlook.com>
2025-01-15 16:14:45 +0800

    rewrite rainfall reader

--- hydrodatasource/reader/rainfall_reader.py
+++ hydrodatasource/reader/rainfall_reader.py
@@ -2,9 +2,9 @@
 Author: liutiaxqabs 1498093445@qq.com
 Date: 2025-01-15 09:39:36
 LastEditors: Wenyu Ouyang
-LastEditTime: 2025-01-15 14:08:05
+LastEditTime: 2025-01-15 16:13:50
 FilePath: \hydrodatasource\hydrodatasource\reader\rainfall_reader.py
-Description: Reader for rainfall data
+Description: Reader for cleaned rainfall data
 """
 
 import collections
@@ -13,6 +13,9 @@ import os
 import pandas as pd
 from tqdm import tqdm
 import xarray as xr
+
+from hydroutils.hydro_log import hydro_logger
+
 from hydrodatasource.reader.data_source import HydroData
 
 
@@ -76,57 +79,67 @@ def filter_out_rows(df1, df2, key_columns):
     return filtered_df
 
 
+@hydro_logger
 class RainfallReader(HydroData):
-    def __init__(self, data_folder, output_folder):
+    def __init__(self, data_folder):
+        """A reader for basin rainfall data
+        Even for station data, we still use basin as the index
+
+        Parameters
+        ----------
+        data_folder : _type_
+            _description_
+        """
         self.data_source_dir = data_folder
-        self.output_folder = output_folder
         self.data_source_description = self.set_data_source_describe()
-        self.pptn_info = self.read_pptn_info()
+        self.station_info = self.read_station_info()
 
     def set_data_source_describe(self):
+        data_source_dir = self.data_source_dir
+        station_rainfall_data_dir = os.path.join(
+            data_source_dir, "station_rainfall_cleaned"
+        )
+        basin_mean_data_dir = os.path.join(data_source_dir, "basin_mean_rainfall")
+        station_info_file = os.path.join(data_source_dir, "stations", "stations.csv")
+        stations_csv_path = os.path.join(data_source_dir, "basins_pp_stations")
+        shp_folder = os.path.join(data_source_dir, "basins_shp")
         return collections.OrderedDict(
-            {
-                "PPTN_INFO_FILE": os.path.join(self.data_source_dir, "pptn_info.csv"),
-            }
+            STATION_RAINFALL_DATA_DIR=station_rainfall_data_dir,
+            BASIN_RAINFALL_DATA_DIR=basin_mean_data_dir,
+            STATION_INFO_FILE=station_info_file,
+            STATIONS_CSV_PATH=stations_csv_path,
+            SHP_FOLDER=shp_folder,
         )
 
-    def read_pptn_info(self):
+    def read_station_info(self):
         return pd.read_csv(
-            self.data_source_description["PPTN_INFO_FILE"], dtype={"STCD": str}
+            self.data_source_description["STATION_INFO_FILE"], dtype={"STCD": str}
         )
 
-    def read_station_rainfall(self, abnormal=False):
+    def read_basin_station_info(self, basin_id):
+        basin_station_info_file = os.path.join(
+            self.data_source_description["STATIONS_CSV_PATH"],
+            f"{basin_id}_stations.csv",
+        )
+        return pd.read_csv(basin_station_info_file, dtype={"STCD": str})
+
+    def read_basin_stations_rainfall(self, basin_id):
         # Create an empty xarray.Dataset
         pptn_rainfall_data = xr.Dataset()
 
         # Iterate over all station IDs in pptn_info with tqdm progress bar
         rainfall_data_list = []
+        basin_station_info = self.read_basin_station_info(basin_id)
         for station_id in tqdm(
-            self.pptn_info["STCD"], desc="Reading station rainfall data"
+            basin_station_info["STCD"], desc="Reading station rainfall data"
         ):
             # Read rainfall data for a single station
-            single_station_rainfall = self.read_1station_rainfall(station_id)
+            single_station_rainfall = self.read_basin_1station_rainfall(
+                basin_id, station_id
+            )
             rainfall_data_list.append(single_station_rainfall)
 
-        # update abnormal data
-        if abnormal == True:
-            abnormal_data = merge_filtered_data_recursive(
-                self.output_folder, ["consistency", "extreme", "gradient"]
-            )
-            rainfall_data_list = filter_out_rows(
-                rainfall_data_list, abnormal_data, ["STCD", "TM"]
-            )
-
-        # Check the earliest date
-        start_date = pd.Timestamp.max
-        for rainfall in rainfall_data_list:
-            if rainfall.index[0] < start_date:
-                start_date = rainfall.index[0]
-
-        # If the start date is too early (before 1980), drop data before 1980
-        if start_date.year < 1980:
-            start_date = pd.Timestamp(year=1980, month=1, day=1)
-
+        start_date = self._get_start_date(rainfall_data_list)
         # Concatenate all rainfall data along the 'STCD' dimension
         rainfall_data = xr.concat(
             [
@@ -139,18 +152,17 @@ class RainfallReader(HydroData):
                     },
                 )
                 for rainfall, station_id in zip(
-                    rainfall_data_list, self.pptn_info["STCD"]
+                    rainfall_data_list, basin_station_info["STCD"]
                 )
             ],
             dim="STCD",
         )
 
-        # Add the rainfall data to the xarray.Dataset
-        pptn_rainfall_data["rainfall"] = rainfall_data
+        pptn_rainfall_data[basin_id] = rainfall_data
 
         return pptn_rainfall_data
 
-    def read_1station_rainfall(self, station_id, abnormal=False):
+    def read_basin_1station_rainfall(self, basin_id, station_id):
         """
         Read rainfall data for a single station.
 
@@ -165,18 +177,87 @@ class RainfallReader(HydroData):
             A DataFrame containing rainfall data for the specified station.
         """
         file_path = os.path.join(
-            self.data_source_dir, station_id, f"pp_CHN_songliao_{station_id}.csv"
+            self.data_source_description["STATION_RAINFALL_DATA_DIR"],
+            basin_id,
+            # TODO: songliao is a hard code -- prefix may change in different data source
+            f"pp_CHN_songliao_{station_id}.csv",
         )
         df = pd.read_csv(
             file_path, parse_dates=["TM"], dtype={"STCD": str, "DRP": float}
         )
-        # update abnormal data
-        if abnormal == True:
-            abnormal_data = merge_filtered_data_recursive(
-                self.output_folder, ["consistency", "extreme", "gradient"]
-            )
-            df = filter_out_rows(df, abnormal_data, ["STCD", "TM"])
-
         df.set_index("TM", inplace=True)
 
         return df
+
+    def read_1basin_rainfall(self, basin_id):
+        """
+        Read basin-mean rainfall data for a single basin.
+
+        Parameters
+        ----------
+        basin_id : str
+            Basin ID
+
+        Returns
+        -------
+        pd.DataFrame
+            A DataFrame containing rainfall data for the specified basin.
+        """
+        basin_mean_file = os.path.join(
+            self.data_source_description["BASIN_RAINFALL_DATA_DIR"],
+            f"{basin_id}_rainfall.csv",
+        )
+        basin_rainfall_data = pd.read_csv(
+            basin_mean_file, parse_dates=["TM"], dtype={"mean_rainfall": float}
+        )
+        # rename col mean_rainfall to rain
+        basin_rainfall_data.rename(columns={"mean_rainfall": "rain"}, inplace=True)
+        # set TM as index
+        basin_rainfall_data.set_index("TM", inplace=True)
+        return basin_rainfall_data
+
+    def read_basin_rainfall(self, basin_ids):
+        """
+        Read rainfall data for all basins.
+
+        Parameters
+        ----------
+        basin_ids : list
+            List of basin IDs
+
+        Returns
+        -------
+        pd.DataFrame
+            A DataFrame containing rainfall data for all basins.
+        """
+        rainfall_data_list = []
+        for basin_id in basin_ids:
+            a_basin_data = self.read_1basin_rainfall(basin_id)
+            rainfall_data_list.append(a_basin_data)
+        start_date = self._get_start_date(rainfall_data_list)
+        return xr.concat(
+            [
+                xr.DataArray(
+                    rainfall.loc[start_date:]["rain"].values,
+                    dims=["time"],
+                    coords={
+                        "time": rainfall.loc[start_date:].index.values,
+                        "basin": station_id,
+                    },
+                )
+                for rainfall, station_id in zip(rainfall_data_list, basin_ids)
+            ],
+            dim="basin",
+        )
+
+    def _get_start_date(self, rainfall_data_list):
+        result = pd.Timestamp.max
+        for rainfall in rainfall_data_list:
+            if rainfall.index[0] < result:
+                result = rainfall.index[0]
+        if result.year < 1980:
+            result = pd.Timestamp(year=1980, month=1, day=1)
+        return result
+
+    def read_basin_rainfall_statistics(self):
+        pass
