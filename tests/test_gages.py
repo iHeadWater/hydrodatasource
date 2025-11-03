7a2037a86f61d2ef96f814ab887f44e8c46d1da9
owen <wenyuouyang@outlook.com>
2025-11-03 17:30:33 +0800

    3 ymls for .github

--- tests/test_gages.py
+++ tests/test_gages.py
@@ -1,7 +1,7 @@
 """
 Author: Wenyu Ouyang
 Date: 2024-11-04 19:50:06
-LastEditTime: 2025-05-14 14:16:19
+LastEditTime: 2025-11-03 17:14:00
 LastEditors: Wenyu Ouyang
 Description: test for gages
 FilePath: \hydrodatasource\tests\test_gages.py
@@ -18,10 +18,10 @@ from hydrodatasource.configs.config import CACHE_DIR, SETTING
 @pytest.fixture
 def gages_dataset():
     # local
-    gages_path = os.path.join(SETTING["local_data_path"]["datasets-origin"], "gages")
+    gages_path = os.path.join(SETTING["local_data_path"]["datasets-interim"])
     # minio
     # gages_path = "s3://basins-interim"
-    return Gages(data_path=gages_path)
+    return Gages(data_path=gages_path, dataset_name="hydrodl-reservoir-jh-paper")
 
 
 def test_gages_read_site_info(gages_dataset):
@@ -31,4 +31,6 @@ def test_gages_read_site_info(gages_dataset):
 
 def test_gages_cache_attributes_xrdataset(gages_dataset):
     gages_dataset.cache_attributes_xrdataset()
-    assert os.path.exists(os.path.join(CACHE_DIR, "attributes.nc"))
+    assert os.path.exists(
+        os.path.join(CACHE_DIR, "hydrodl-reservoir-jh-paper_attributes.nc")
+    )
